7f9e94923d52e39c6569ddc35da3ba0c8c69d3a9
Thibault FRANCOIS <thifranc@e1r13p2.42.fr>
2016-08-19 11:12:46 +0200

    just saving

---
+++ bubble_sort.c
@@ -0,0 +1,28 @@
+
+//BUBBLE SORT :
+
+/*
+t_list		*next_a_elem(t_list **list, t_data data)
+{
+	t_list	*tmp;
+	
+	tmp = *list;
+	while (belongs_to(tmp, data.goal, data.pivot) < 0)
+		tmp = tmp->next;
+	return (tmp);
+}
+*/
+				/*if (*a == list_max(&(*a)))
+					rotate(&(*a));
+				else
+				{
+					if ((*a)->nbr > next_a_elem(&((*a)->next), data)->nbr)
+						swapp(&(*a));
+					else
+					{
+						rotate(&(*a));
+					}
+				}
+
+				
+*/
